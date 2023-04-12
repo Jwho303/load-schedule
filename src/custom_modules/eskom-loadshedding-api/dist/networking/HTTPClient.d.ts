cb4c638eef72a79597981c969319e7ec5fcbe1e9
Jeff <jwho303@gmail.com>
2023-04-12 21:49:33 +0200

    buggering around

--- src/custom_modules/eskom-loadshedding-api/dist/networking/HTTPClient.d.ts
+++
@@ -1,5 +0,0 @@
-import { AxiosResponse } from 'axios';
-export default class HTTPClient {
-    protected static BASE_URL: string;
-    protected static get(path: string, addTime?: boolean): Promise<AxiosResponse<any>>;
-}
--- src/custom_modules/eskom-loadshedding-api/dist/parser/ScheduleParser.d.ts
+++
@@ -1,2 +0,0 @@
-import { LoadsheddingSchedule } from '../models/LoadsheddingSchedule';
-export declare function scheduleParser(scheduleText: string): LoadsheddingSchedule;
