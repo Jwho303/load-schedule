20d858675d221d67678e0949ab8e1c77f7274128
Jeff <jwho303@gmail.com>
2023-04-12 17:58:19 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import data from "./data.json";
+import Schedule from "./Schedule";
+import { Person } from "./types";
+import TestPage from "./TestPage";
+
+const App = () => {
+  const [people, setPeople] = useState<Person[]>(data);
+
+  const handleAddPerson = (person: Person) => {
+    setPeople([...people, person]);
+  };
+
+  return (
+    <div>
+      <h1>Loadshedding Schedule App</h1>
+      <h2>Schedule</h2>
+      <TestPage />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Schedule.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import data from "./data.json";
+import { Person } from "./types";
+
+const isAffected = (person: Person, time: string) => {
+  // determine if the person is affected by loadshedding at the given time
+  // you can use a library or API to get the loadshedding schedule for the person's area
+  // and compare it to the current time
+  return true;
+};
+
+const Schedule = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const rows = [];
+  for (let i = 0; i < 48; i++) {
+    const time = `${Math.floor(i / 2)
+      .toString()
+      .padStart(2, "0")}:${(i % 2 === 0 ? "00" : "30")}`;
+    const cells = data.map((person) => (
+      <td key={person.name}>
+        {/* determine if the person is affected by loadshedding at this time */}
+        {isAffected(person, time) ? "X" : "-"}
+      </td>
+    ));
+    rows.push(
+      <tr key={time}>
+        <td>{time}</td>
+        {cells}
+      </tr>
+    );
+  }
+
+
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Time</th>
+          {data.map((person) => (
+            <th key={person.name}>{person.name}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+};
+
+export default Schedule;
